027370af5e90a5aa1b133000414e8c8483d0230d
SivaSaran <SivaSaran@SivaSarans-MacBook-Pro.local>
2015-11-06 13:26:45 -0800

    Initial Commit

---
+++ Acronym/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Acronym
+//
+//  Created by SivaSaran on 11/6/15.
+//  Copyright (c) 2015 SivaSaran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Acronym/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Acronym
+//
+//  Created by SivaSaran on 11/6/15.
+//  Copyright (c) 2015 SivaSaran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
